1a0054d28fac5f2731b23b3d0817f6cd354ff9c4
sarbani86 <123646208+sarbani86@users.noreply.github.com>
2023-02-27 16:33:07 +0530

    Update streamlit_app.py
    only show the dataframe filter by selected fruit item

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,9 @@ my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw
 #want to choose the friut by name
 my_fruit_list=my_fruit_list.set_index('Fruit')
 #lets pick the fruit
-streamlit.multiselect("choose ur fruit : ",list(my_fruit_list.index),['Apple','Banana'])
-streamlit.dataframe(my_fruit_list)
+fruit_selected=streamlit.multiselect("choose ur fruit : ",list(my_fruit_list.index),['Apple','Banana'])
+fruit_show=my_fruit_list.loc[fruit_selected]
+streamlit.dataframe(fruit_show)
 
 
 
