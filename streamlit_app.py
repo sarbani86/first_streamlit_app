ca0d957ad4156cd2f3e521cf5b08caa401089e65
sarbani86 <123646208+sarbani86@users.noreply.github.com>
2023-02-27 15:14:46 +0530

    Update streamlit_app.py
    added another header with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ streamlit.title('Look !!my first app')
 streamlit.header('Breakfast Manu')
 streamlit.text('Omega 3 & Blue berry Oat Meal')
 streamlit.text('Kale ,Spinach & Rocket smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
