0c5708e072d55b6f12be77f8f6f7ebc2d84e50d3
sarbani86 <123646208+sarbani86@users.noreply.github.com>
2023-02-27 16:05:18 +0530

    Update streamlit_app.py
    added pandas to incorporate dataframe into the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
+import pandas as pd
 streamlit.title('Look !!my first app')
 streamlit.header('Breakfast Manu')
 streamlit.text('Omega 3 & Blue berry Oat Meal')
 streamlit.text('Kale ,Spinach & Rocket smoothie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
 
