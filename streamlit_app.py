d0e46c24dace838910c07315bff4813aff476c2d
sarbani86 <123646208+sarbani86@users.noreply.github.com>
2023-02-27 16:10:29 +0530

    Update streamlit_app.py
    adding user interactive widgets

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ streamlit.text('Omega 3 & Blue berry Oat Meal')
 streamlit.text('Kale ,Spinach & Rocket smoothie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#lets pick the fruit
+streamlit.multiselect("choose ur fruit : ",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
