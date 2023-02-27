ae56a0076c0f0fd5c309294dd1fa036279555046
sarbani86 <123646208+sarbani86@users.noreply.github.com>
2023-02-27 16:25:42 +0530

    Update streamlit_app.py
    adding fruits name

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw
 #want to choose the friut by name
 my_fruit_list=my_fruit_list.set_index('Fruit')
 #lets pick the fruit
-streamlit.multiselect("choose ur fruit : ",list(my_fruit_list.index))
+streamlit.multiselect("choose ur fruit : ",list(my_fruit_list.index),['Apple','Banana'])
 streamlit.dataframe(my_fruit_list)
 
 
